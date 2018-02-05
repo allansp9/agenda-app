925f383b7cf77affca5bfb2913736fbf05754b83
Allan <allansp9@gmail.com>
2018-02-05 17:33:44 -0200

    added search component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Search from './Search';
 
 const Header = () => (
   <div>
-    <h1>Header</h1>
+    <Search />
   </div>
 );
 
---
+++ src/components/Search.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Search = () => (
+  <div>
+    <input type="text" placeholder="Buscar" />
+  </div>
+);
+
+export default Search;
