c957959de7ab5f8c3dfa12ab309862e3e3ddb133
Allan <allansp9@gmail.com>
2018-02-09 21:10:52 -0200

    adds transition from add to info page

--- src/components/AddContato.js
+++ src/components/AddContato.js
@@ -2,11 +2,17 @@ import React from 'react';
 import { connect } from 'react-redux';
 import FormContato from './FormContato';
 import { addContato } from '../actions/contatos';
+import setActiveAction from '../actions/active';
+
+const adicionarContato = (props) => {
+  props.dispatch(addContato(props.data));
+  props.dispatch(setActiveAction('info', props.data));
+};
 
 const AddContato = props => (
   <div>
     <h1>Adicionando</h1>
-    <FormContato onSubmit={data => props.dispatch(addContato(data))} />
+    <FormContato onSubmit={data => adicionarContato({ ...props, data })} />
   </div>
 );
 
