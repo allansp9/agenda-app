906115606073211d0cbe3a8375384eee93cd9092
Allan <allansp9@gmail.com>
2018-02-14 20:44:31 -0200

    Refatora código no ListaContatos

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { selectContatos } from '../selectors/contatos';
 import Dashboard from './Dashboard';
 import Header from './Header';
 import ListaContatos from './ListaContatos';
+import PreviewContato from './PreviewContato';
 import Footer from './Footer';
 
-const App = () => (
+const App = ({ contatos }) => (
   <div>
     <Header />
-    <ListaContatos />
+    <ListaContatos>
+      {contatos.map(contato => <PreviewContato key={contato.id} {...contato} />)}
+    </ListaContatos>
     <Dashboard />
     <Footer />
   </div>
 );
 
-export default App;
+const mapStateToProps = state => ({
+  contatos: selectContatos(state.contatos, state.filter.text),
+});
+
+export default connect(mapStateToProps)(App);
--- src/components/ListaContatos.js
+++ src/components/ListaContatos.js
@@ -1,19 +1,5 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import PreviewContato from './PreviewContato';
-import { selectContatos } from '../selectors/contatos';
-import setActive from '../actions/active';
 
-const ListaContatos = ({ contatos }) => (
-  <div>{contatos.map(contato => <PreviewContato key={contato.id} {...contato} />)}</div>
-);
+const ListaContatos = ({ children }) => <div>{children}</div>;
 
-const mapStateToProps = state => ({
-  contatos: selectContatos(state.contatos, state.filter.text),
-});
-
-const mapDispatchToProps = dispatch => ({
-  setActive: active => dispatch(setActive(active)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(ListaContatos);
+export default ListaContatos;
