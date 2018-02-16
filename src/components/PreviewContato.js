fa27924924621d94ef4546a559155f7ed19609df
Allan <allansp9@gmail.com>
2018-02-16 14:12:49 -0200

    coloca background no contato ativo

--- src/components/PreviewContato.js
+++ src/components/PreviewContato.js
@@ -2,40 +2,29 @@ import React from 'react';
 import { connect } from 'react-redux';
 import setActive from '../actions/active';
 
-class PreviewContato extends React.Component {
-  state = {
-    foto: '',
-  };
-
-  render() {
-    const { ...contato } = this.props;
-    return (
-      <div
-        onClick={() => contato.dispatch(setActive('info', contato.id))}
-        role="button"
-        tabIndex={contato.id}
-        onKeyUp={() => contato.dispatch(setActive('info', contato.id))}
-        className="lista-contatos__item"
-      >
-        <div className="lista-contatos__foto">
-          <img
-            src={
-              !this.props.foto ? require('../../public/images/placeholder.png') : this.props.foto
-            }
-            alt={contato.nome}
-          />
-        </div>
-        <div>
-          <div>
-            <span className="lista-contatos__nome">{contato.nome}</span>
-          </div>
-          <div>
-            <span className="lista-contatos__email">{contato.email}</span>
-          </div>
-        </div>
+const PreviewContato = ({ activeUser, ...contato }) => (
+  <div
+    onClick={() => contato.dispatch(setActive('info', contato.id))}
+    role="button"
+    tabIndex={contato.id}
+    onKeyUp={() => contato.dispatch(setActive('info', contato.id))}
+    className={`lista-contatos__item ${contato.id === activeUser ? 'is-active' : ''}`}
+  >
+    <div className="lista-contatos__foto">
+      <img
+        src={!contato.foto ? require('../../public/images/placeholder.png') : contato.foto}
+        alt={contato.nome}
+      />
+    </div>
+    <div>
+      <div>
+        <span className="lista-contatos__nome">{contato.nome}</span>
       </div>
-    );
-  }
-}
+      <div>
+        <span className="lista-contatos__email">{contato.email}</span>
+      </div>
+    </div>
+  </div>
+);
 
-export default connect()(PreviewContato);
+export default connect(state => ({ activeUser: state.active.activeUser }))(PreviewContato);
