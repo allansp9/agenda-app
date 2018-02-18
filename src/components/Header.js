61af747e84d289209a3a695517a91ee87cb99442
Allan <allansp9@gmail.com>
2018-02-18 18:07:40 -0300

    Adiciona proptypes ao Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Header = ({ children }) => <header className="aside__header">{children}</header>;
+
+Header.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default Header;
