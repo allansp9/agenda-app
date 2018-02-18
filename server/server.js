ab0b56401af173a40545017b5b4af8755ccbfbce
Allan <allansp9@gmail.com>
2018-02-18 19:10:51 -0300

    remove codigo sem uso do webpack

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
@@ -10,6 +11,4 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log('Server is up!');
-});
+app.listen(port, () => {});
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,5 @@
 const path = require('path');
-const webpack = require('webpack');
-const dotenv = require('dotenv');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
-if (process.env.NODE_ENV === 'test') {
-  dotenv.config({ path: '.env.test' });
-} else if (process.env.NODE_ENV === 'development') {
-  dotenv.config({ path: '.env.development' });
-}
 
 module.exports = (env) => {
   const isProduction = env === 'production';
