8f4d59cf3b41316aa0899ac99f0cfcf19624db8e
Allan <allansp9@gmail.com>
2018-02-18 18:10:08 -0300

    Adiciona proptypes ao InfoContato

--- src/components/InfoContato.js
+++ src/components/InfoContato.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import Header from './Header';
 import setActive from '../actions/active';
@@ -38,6 +39,11 @@ const InfoContato = (props) => {
       </div>
     </div>
   );
+};
+
+InfoContato.propTypes = {
+  contato: PropTypes.objectOf(PropTypes.node).isRequired,
+  dispatch: PropTypes.func.isRequired,
 };
 
 export default connect()(InfoContato);
