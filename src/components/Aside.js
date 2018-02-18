cd08cac7a449bfa2b0c796257fa782ce15d5e488
Allan <allansp9@gmail.com>
2018-02-18 17:48:17 -0300

    Adiciona proptypes ao Aside

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 const Aside = ({ children, dispatch, activePage }) => (
   <div className={`aside ${activePage && 'is-active-mobile'} `}>{children}</div>
 );
+
+Aside.propTypes = {
+  activePage: PropTypes.string.isRequired,
+  children: PropTypes.arrayOf(PropTypes.node).isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
 
 export default connect()(Aside);
